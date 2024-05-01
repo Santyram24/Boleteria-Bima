56145ca15aaf72f4f14773c869a4ba4654c157f7
jube23 <152728203+jube23@users.noreply.github.com>
2024-04-30 20:54:15 -0500

    primer commit

---
+++ Paginas/LOTERIA/index.js
@@ -0,0 +1,34 @@
+	function btnx2(){
+
+		var presio = document.getElementById('txtpreciorif0').innerHTML = "3.000"; 
+
+
+	}
+
+		function btnx4(){
+
+		var presio = document.getElementById('txtpreciorif0').innerHTML = "6.000"; 
+		
+
+	}
+
+			function btnx6(){
+
+		var presio = document.getElementById('txtpreciorif0').innerHTML = "9.000"; 
+		
+
+	}
+
+			function btnx8(){
+
+		var presio = document.getElementById('txtpreciorif0').innerHTML = "12.000"; 
+		
+
+	}
+
+			function btnx10(){
+
+		var presio = document.getElementById('txtpreciorif0').innerHTML = "15.000"; 
+		
+
+	}
